13b0f5a33c521b5521ee2dad130c4d8c981bdf74
Mister-Corn <code@ronaldlibago.com>
2018-12-16 10:01:02 -0800

    Develop further functional test to test input of a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,18 +20,34 @@ class NewVisitorTest(unittest.TestCase):
     # structure: assertion to test, message when test fails
     # OLD: assert 'To-Do' in browser.title, "Browser title was " + browser.title
     self.assertIn('To-Do', self.browser.title)
-    self.fail('Finish the test!')
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
 
     # Schmo is invited to enter a to-do item straight away
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
 
     # Schmo types "Get a life" into a text box
+    inputbox.send_keys('Get a life')
 
     # Schmo hits enter, the page updates, and the page lists
     # "1: Get a life" as an item in the to-do list
+    inputbox.set_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1: Get a life' for row in rows)
+    )
 
     # The text box persists, ever hungry for more inane tasks for 
     # schmo to keep track of. Schmo enters another tasks:
     # "Made dat money"
+    self.fail('Test ain\'t finished, bruh')
 
     # Page updates again, and now shows both items in the list
 
